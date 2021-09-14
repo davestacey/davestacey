eae4b77398b35e7620805163af9df0ce32ab197f
David <davestacey@users.noreply.github.com>
2021-09-13 21:32:41 -0400

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,83 @@
+import pkg from './package'
+
+const { CI_PAGES_URL } = process.env
+const base = CI_PAGES_URL && new URL(CI_PAGES_URL).pathname
+
+export default {
+  mode: 'universal',
+
+  target: 'static',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Customize the generated output folder
+  */
+  generate: {
+    dir: 'docs'
+  },
+
+  /*
+  ** Customize the base url
+  */
+  router: {
+    // base
+    base: '/davestacey/'
+  },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="container">
+    <div>
+      <!-- <logo /> -->
+      <h1 class="title">
+        Dave Stacey
+      </h1>
+      <h2>
+        (dev nuxt)
+      </h2>
+      <article>
+        Dev notes to come
+      </article>
+      <div class="links">
+        <a
+          href="https://github.com/davestacey"
+          target="_blank"
+          class="button--grey"
+        >GitHub
+        </a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 40px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 32px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+  margin: 0;
+}
+</style>
